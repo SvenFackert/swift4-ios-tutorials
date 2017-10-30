7024681b0f266e5808862cee7636505a34896ea3
sven.fackert <sven.fackert@ergosign.de>
2017-10-30 11:19:13 +0100

    Vorlesung 4 Auto Layout Introduction

---
+++ 01-20_SwiftBasics.playground/Pages/VariablesVsConstants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let str = "Hello, playground"
+let myString: String = "Hello"
+
+// Multi line strings - since swift 4
+let multiLineString = """
+This is a very long String that covers multiple lines. This is a very long String that covers multiple lines. This is a very long String that covers multiple lines. This is a very long String that covers multiple lines. This is a very long String that covers multiple lines.
+"""
